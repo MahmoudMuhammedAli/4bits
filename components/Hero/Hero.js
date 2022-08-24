7d80fe5459f343dc49e4ca055cad023ea65ab2bf
Mohamed Ahmed <83082886+mohamed3omar@users.noreply.github.com>
2022-08-24 02:45:03 +0200

    some changes

--- components/Hero/Hero.js
+++ components/Hero/Hero.js
@@ -1,6 +1,10 @@
-import HeroTitle from "./HeroTitle";
+// import HeroTitle from "./HeroTitle";
 import Image from "next/image";
 // import randomImage from "../../assets/img/randomImage.jpg";
+import dynamic from "next/dynamic";
+
+const HeroTitle = dynamic(()=>import('./HeroTitle'));
+
 export default function Hero() {
   return (
     <div className="flex flex-col w-[100%]  my-30">
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
-import Hero from "../components/Hero/Hero";
-import Projects from "../components/Sections/Projects";
-import Team from "../components/Sections/Team";
+// import Hero from "../components/Hero/Hero";
+// import Projects from "../components/Sections/Projects";
+// import Team from "../components/Sections/Team";
 import WhatDoing from "../components/WhatDoing/WhatDoing";
-import Testimonials from "../components/Sections/Testimonials";
-
+// import Testimonials from "../components/Sections/Testimonials";
+import dynamic from "next/dynamic";
+const Team = dynamic(()=>import('../components/Sections/Team')) ;
+const Projects = dynamic(()=>import('../components/Sections/Projects')) ;
+const Hero = dynamic(()=>import('../components/Hero/Hero')) ;
+const Testimonials = dynamic(()=>import('../components/Sections/Testimonials')) ;
 
 export default function Home() {
   return (
