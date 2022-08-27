f51d3dba138f821d61e79e3b46b7c55b82fdcd07
Abdelrahman Mohamed <80596612+AbdElrahmanBEBO@users.noreply.github.com>
2022-08-27 18:47:38 +0200

    contact us

--- pages/index.js
+++ pages/index.js
@@ -3,19 +3,19 @@ import Projects from "../components/Sections/Projects";
 import Team from "../components/Sections/Team";
 import WhatDoing from "../components/WhatDoing/WhatDoing";
 import Testimonials from "../components/Sections/Testimonials";
-
-
+import Contact from "../components/ContactUs/ContactUs"
+  
 export default function Home() {
   return (
-    <div className="overflow-x-hidden">
-      <div className="flex items-center justify-center py-40">
-        <Hero />
-      </div>
-      <Team />
-      <Testimonials />
-      <Projects />
-      <WhatDoing />
-    </div>
-
+    // <div classNameName="overflow-x-hidden">
+    //   <Hero />
+    //   <Team />
+    //   <Testimonials />
+    //   <Projects />
+    //   <WhatDoing />
+    // </div>
+    <>
+      <Contact />
+    </>
   );
 }
