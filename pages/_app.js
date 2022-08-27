3ae3cd4eced990930520117ac9ab96925eda06a7
Abdelrahman Mohamed <80596612+AbdElrahmanBEBO@users.noreply.github.com>
2022-08-27 23:13:37 +0200

    contact us

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,8 @@
 import "../styles/globals.css";
 import "../styles/style.css";
+import "react-phone-number-input/style.css";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import "react-phone-number-input/style.css";
-
-
 
 function MyApp({ Component, pageProps }) {
   return (
--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,11 @@
-// import Hero from "../components/Hero/Hero";
-// import Projects from "../components/Sections/Projects";
-// import Team from "../components/Sections/Team";
-// import Testimonials from "../components/Sections/Testimonials";
+import Hero from "../components/Hero/Hero";
+import Projects from "../components/Sections/Projects";
+import Team from "../components/Sections/Team";
+import Testimonials from "../components/Sections/Testimonials";
 import WhatDoing from "../components/WhatDoing/WhatDoing";
 import Contact from "../components/ContactUs/Contact.js"
 
-import dynamic from "next/dynamic";
-const Team = dynamic(()=>import('../components/Sections/Team')) ;
-const Projects = dynamic(()=>import('../components/Sections/Projects')) ;
-const Hero = dynamic(()=>import('../components/Hero/Hero')) ;
-const Testimonials = dynamic(()=>import('../components/Sections/Testimonials')) ;
+
 
 export default function Home() {
   return (
