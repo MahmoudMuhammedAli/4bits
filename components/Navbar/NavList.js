0bf7d378f9e5a1257a0395126add729f9088d733
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-08-28 16:14:34 +0200

    Update NavList.js

--- components/Navbar/NavList.js
+++ components/Navbar/NavList.js
@@ -19,52 +19,61 @@ export default function NavList(props) {
         : "text-black" &&
           "bg-white"} flex items-center justify-between flex-shrink-0 gap-10`}
     >
-      <div className="flex flex-row gap-4">
-        <li
-          className={`font-semibold text-lg md:text-xl lg:text-2xl transition-all delay-75  duration-500 ${props.toggle
-            ? "px-12 py-3 border-b-2 shadow-sm -translate-x-0"
-            : props.responsive ? "-translate-x-[100vw]" : "translate-x-0"}`}
-        >
-          Home
-        </li>
-        <li
-          className={`font-semibold text-lg md:text-xl lg:text-2xl transition-all delay-100 duration-500 ${props.toggle
-            ? "px-12 py-3 border-b-2 shadow-sm -translate-x-0"
-            : props.responsive ? "-translate-x-[100vw]" : "translate-x-0"}`}
-        >
-          Team
-        </li>
-        <li
-          className={`font-semibold text-lg md:text-xl lg:text-2xl transition-all delay-150 duration-500 ${props.toggle
-            ? "px-12 py-3 border-b-2 shadow-sm -translate-x-0"
-            : props.responsive ? "-translate-x-[100vw]" : "translate-x-0"}`}
-        >
-          Projects
-        </li>
-        <li
-          className={`font-semibold text-lg md:text-xl lg:text-2xl transition-all delay-200 duration-500 ${props.toggle
-            ? "px-12 py-3 border-b-2 shadow-sm -translate-x-0"
-            : props.responsive ? "-translate-x-[100vw]" : "translate-x-0"}`}
-        >
-          About
-        </li>
-        <li
-          className={`font-semibold text-lg md:text-xl lg:text-2xl transition-all delay-300 duration-500 ${props.toggle
-            ? "px-12 py-3 border-b-2 shadow-sm -translate-x-0"
-            : props.responsive ? "-translate-x-[100vw]" : "translate-x-0"}`}
-        >
-          Contact
-        </li>
-
+      <li
+        className={`font-semibold w-full text-lg md:text-xl lg:text-2xl transition-all delay-75  duration-500 ${props.toggle
+          ? "px-12 py-3 border-b-2 shadow-sm -translate-x-0"
+          : props.responsive ? "-translate-x-[100vw]" : "translate-x-0"}`}
+      >
+        Home
+      </li>
+      <li
+        className={`font-semibold w-full text-lg md:text-xl lg:text-2xl transition-all delay-100 duration-500 ${props.toggle
+          ? "px-12 py-3 border-b-2 shadow-sm -translate-x-0"
+          : props.responsive ? "-translate-x-[100vw]" : "translate-x-0"}`}
+      >
+        Team
+      </li>
+      <li
+        className={`font-semibold w-full text-lg md:text-xl lg:text-2xl transition-all delay-150 duration-500 ${props.toggle
+          ? "px-12 py-3 border-b-2 shadow-sm -translate-x-0"
+          : props.responsive ? "-translate-x-[100vw]" : "translate-x-0"}`}
+      >
+        Projects
+      </li>
+      <li
+        className={`font-semibold w-full text-lg md:text-xl lg:text-2xl transition-all delay-200 duration-500 ${props.toggle
+          ? "px-12 py-3 border-b-2 shadow-sm -translate-x-0"
+          : props.responsive ? "-translate-x-[100vw]" : "translate-x-0"}`}
+      >
+        About
+      </li>
+      <li
+        className={`font-semibold w-full text-lg md:text-xl lg:text-2xl transition-all delay-300 duration-500 ${props.toggle
+          ? "px-12 py-3 border-b-2 shadow-sm -translate-x-0"
+          : props.responsive ? "-translate-x-[100vw]" : "translate-x-0"}`}
+      >
+        Contact
+      </li>
+      <li
+        className={`font-semibold w-full text-lg md:text-xl lg:text-2xl transition-all delay-300 duration-500 ${props.toggle
+          ? "px-12 py-3 border-b-2 shadow-sm -translate-x-0"
+          : props.responsive ? "-translate-x-[100vw]" : "translate-x-0"}`}
+      >
         <Link href="/" locale={router.locale === "en" ? "ar" : "en"}>
-          <button className="w-40 rounded-xl bg-prim text-white font-semibold py-1">
+          <button className="w-40 rounded-xl bg-prim text-white font-semibold w-full py-1">
             {t("change-locale", {
               changeTo: router.locale === "en" ? "ar" : "en",
             })}
           </button>
         </Link>
-      </div>
-      <ThemeChanger />
+      </li>
+      <li
+        className={`font-semibold w-full text-lg md:text-xl lg:text-2xl transition-all delay-300 duration-500 ${props.toggle
+          ? "px-12 py-3 border-b-2 shadow-sm -translate-x-0"
+          : props.responsive ? "-translate-x-[100vw]" : "translate-x-0"}`}
+      >
+        <ThemeChanger />
+      </li>
     </ul>
   );
 }
