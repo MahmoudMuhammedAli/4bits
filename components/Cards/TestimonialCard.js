83fe9d4bc3997172d622461d8453d69718b1cdaf
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-08-28 05:29:26 +0200

    dragable carousel

--- components/Cards/TestimonialCard.js
+++ components/Cards/TestimonialCard.js
@@ -1,14 +1,12 @@
 export default function TestimonialCard(props) {
   return (
-    <div
-      className="flex justify-center items-stretch grow flex-shrink-0 mx-7 min-w-[300px]
+    <div className="flex justify-center items-stretch  flex-shrink-0 mx-7 min-w-[300px]
       bg-center transition-transform duration-200 ease-in-out
-      sm:w-full md:w-6/12 lg:w-4/12 xl:w-2/12 2xl:w-1/12 max-h rounded-2xl hover:scale-[1.05]"
-    >
+      sm:w-full md:w-6/12 lg:w-4/12 xl:w-2/12 2xl:w-1/12 max-h rounded-2xl hover:scale-[1.05]">
       <figure className="snip1157 p-5 rounded-2xl">
         <blockquote className="text-md text-white">
           {props.comment}
-          <div className="arrow"></div>
+          <div className="arrow" />
         </blockquote>
         <img src={props.image} alt="sq-sample3" />
         <div className="author">
--- components/Sections/Testimonials.js
+++ components/Sections/Testimonials.js
@@ -2,9 +2,10 @@ import React from "react";
 import Lottie from "lottie-react";
 import swipe from "../../assets/swipe.json";
 import TestimonialCard from "../Cards/TestimonialCard";
+import { Carousel } from "flowbite-react";
 
 export default function Testimonials(props) {
-  const [hideMouseDetect, setMouseDetect] = React.useState(false);
+  const [ hideMouseDetect, setMouseDetect ] = React.useState(false);
 
   const testimonials = {
     image: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/331810/sq-sample3.jpg",
@@ -16,23 +17,22 @@ export default function Testimonials(props) {
   return (
     <div className="top-[8vh] w-screen ">
       <div className="text-3xl font-bold text-gray-800 my-5 flex justify-start">
-        <div className="border-b-8 border-sec w-[50vw] mb-4"></div>
-        <div className="w-[200px] my-2 mx-16 text-sec">
-          What Clinets Says
-        </div>
+        <div className="border-b-8 border-sec w-[50vw] mb-4" />
+        <div className="w-[200px] my-2 mx-16 text-sec">What Clinets Says</div>
       </div>
       <div className="flex items-center justify-start 2xl:justify-center ">
         <div className="overflow-y-hidden flex flex-row items-center w-full py-10 overflow-x-hidden">
-          <TestimonialCard {...testimonials} />
-          <TestimonialCard {...testimonials} />
-          <TestimonialCard {...testimonials} />
-          <TestimonialCard {...testimonials} />          
-          <TestimonialCard {...testimonials} />          
-          <TestimonialCard {...testimonials} />          
+          <Carousel slide slideInterval={1000}>
+            <TestimonialCard {...testimonials} />
+            <TestimonialCard {...testimonials} />
+            <TestimonialCard {...testimonials} />
+            <TestimonialCard {...testimonials} />
+            <TestimonialCard {...testimonials} />
+            <TestimonialCard {...testimonials} />
+          </Carousel>
           <div
-            className={`absolute flex justify-center items-center w-full h-[100%] sm:hidden ${
-              hideMouseDetect && "hidden"
-            }`}
+            className={`absolute flex justify-center items-center w-full h-[100%] sm:hidden ${hideMouseDetect &&
+              "hidden"}`}
             onTouchStart={() => {
               setMouseDetect(!hideMouseDetect);
             }}
@@ -43,7 +43,7 @@ export default function Testimonials(props) {
               className="-z-100 w-80 rounded-3xl"
             />
           </div>
-          <div className="absolute mt-[-200px] h-44 left-0 w-[20vw] bg-sec z-[-1000] animate-[animateBoxProjects_8s_ease-in-out_infinite_alternate]"></div>
+          <div className="absolute mt-[-200px] h-44 left-0 w-[20vw] bg-sec z-[-1000] animate-[animateBoxProjects_8s_ease-in-out_infinite_alternate]" />
         </div>
       </div>
     </div>
