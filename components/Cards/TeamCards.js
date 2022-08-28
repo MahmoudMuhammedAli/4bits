5d9ddffaca147424b28126967d54ac00a5c84b47
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-08-28 04:33:17 +0200

    restore card style

--- components/Cards/TeamCards.js
+++ components/Cards/TeamCards.js
@@ -11,9 +11,11 @@ export default function TeamCard(props) {
   return (
     <div
       className={`h-[450px] ${theme === "dark"
-        ? "text-black"
-        : "text-black"} bg-white flex items-stretch grow flex-shrink-0 mx-7 w-[130vw] sm:w-full md:w-10/12 lg:w-6/12 xl:w-4/12 2xl:w-3/12 max-h rounded-2xl`}
-      style={{ boxShadow: "5px 5px 20px #ccc" }}
+        ? "text-gray-100 bg-slate-900 "
+        : "text-black bg-white"}  flex items-stretch  flex-shrink-0 mx-7 w-[130vw] sm:w-full md:w-10/12 lg:w-5/12 max-h rounded-2xl `}
+      style={{
+        boxShadow: theme === "dark" ? "5px 5px 20px #ddd" : "5px 5px 20px #222",
+      }}
     >
       <div className="flex w-[150%]">
         <Image
@@ -24,6 +26,7 @@ export default function TeamCard(props) {
       </div>
       <div className="flex flex-col p-8">
         <h1 className="text-3xl font-bold w-[70%] mb-5">{props.name}</h1>
+        <p className="text-gray-400"> - Software Engineer</p>
         <p className="p-5 text-xl font-medium text-gray-600">{props.body}</p>
       </div>
     </div>
