84663cef5f48876c941fd79d83a76f6d919a3302
Abdelrahman Mohamed <80596612+AbdElrahmanBEBO@users.noreply.github.com>
2022-08-29 12:03:42 +0200

    h

--- components/Navbar/NavList.js
+++ components/Navbar/NavList.js
@@ -12,7 +12,13 @@ export default function NavList(props) {
   const router = useRouter();
   useEffect(() => setMounted(true), []);
   if (!mounted) return null;
-  const menuList = ["Home", "Team", "Projects", "About", "Contact"]
+  const menuList = [
+    { name: "Home", delay: 75 },
+    { name: "Team", delay: 200 },
+    { name: "Projects", delay: 300 },
+    { name: "About", delay: 500 },
+    { name: "Contact", delay: 700 },
+  ];
   return (
     <ul
       className={`${props.parentDiv} ${
@@ -26,7 +32,7 @@ export default function NavList(props) {
           <li
             key={index}
             className={`
-             font-semibold text-lg md:text-xl lg:text-2xl transition-all delay-75 duration-500 
+             font-semibold text-lg md:text-xl lg:text-2xl transition-all duration-500 delay-${item.delay}
              ${
                props.toggle
                  ? "px-12 py-3 border-b-2 shadow-sm -translate-x-0"
@@ -35,7 +41,7 @@ export default function NavList(props) {
                  : "translate-x-0"
              }`}
           >
-            {item}
+            {item.name}
           </li>
         );
       })}
@@ -43,11 +49,16 @@ export default function NavList(props) {
       <li
         className={`
           font-semibold flex justify-evenly w-full text-lg md:text-xl lg:text-2xl transition-all delay-300 duration-500 
-          ${props.toggle ? "px-12 py-3 border-b-2 shadow-sm -translate-x-0" : props.responsive ? "-translate-x-[100vw]" : "translate-x-0"
-        }`}
+          ${
+            props.toggle
+              ? "px-12 py-3 border-b-2 shadow-sm -translate-x-0"
+              : props.responsive
+              ? "-translate-x-[100vw]"
+              : "translate-x-0"
+          }`}
       >
         <Link href="/" locale={router.locale === "en" ? "ar" : "en"}>
-          <button className="w-40 rounded-xl bg-prim text-white font-semibold py-1">
+          <button className="w-20 rounded-xl bg-prim text-white font-semibold py-1">
             {t("change-locale", {
               changeTo: router.locale === "en" ? "ar" : "en",
             })}
